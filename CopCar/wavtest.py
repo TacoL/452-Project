fd94498ae44610fab45af6c15b00fe7e462778be
tlucero <tlucero@umich.edu>
2024-10-21 00:32:42 -0400

    confirmed the speaker code works

--- CopCar/wavtest.py
+++ CopCar/wavtest.py
@@ -1,17 +1,17 @@
-# Below are a couple methods of attempting to play the audio file
-# None work, I was also having issues simply instaling pygame, although this might not be necessary once we have the audio jack
-# Theres a decent amount of sample code for playing with the audio jack, also worth looking at
+# This code works, but the speaker we have may need an amplifier
 
+import pygame
 
-# import os
-# os.system('aplay /home/pi/Downloads/Siren.wav')
+def playSound(filename):
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
 
-# def playSound(filename):
-#     pygame.mixer.music.load(filename)
-#     pygame.mixer.music.play()
-# def audio_player():
-#     pygame.init()
-#     playSound('Siren.wav')
-#     while pygame.mixer.music.get_busy() == True:
-#         continue
-#     return
+def audio_player():
+
+    pygame.init()
+    playSound('CopCar/Siren.wav')
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    return
+
+audio_player()
