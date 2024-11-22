822a82bad15113ef115c5ff82d3d82086a20f0b4
TacoL <34144367+TacoL@users.noreply.github.com>
2024-11-22 15:54:35 -0500

    created sample mic in arduino

--- RobberCar/arduino_audio/arduino_audio.ino
+++ RobberCar/arduino_audio/arduino_audio.ino
@@ -1,21 +1,25 @@
 const int sampleWindow = 50;  // Sample window width in mS (50 mS = 20Hz)
 int const AMP_PIN = A0;       // Preamp output pin connected to A0
-unsigned int sample;
+
+unsigned int signalLength = 1095;
+unsigned int completeSignal[signalLength];
+unsigned int * signalIdx = &completeSignal;
 
 void setup()
 {
   Serial.begin(9600);
 }
 
-void loop()
+unsigned int sampleOnce()
 {
-  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int sample = 0;
   unsigned int peakToPeak = 0;   // peak-to-peak level
 
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
 
   // collect data for 50 mS and then plot data
+  unsigned long startMillis = millis(); // Start of sample window
   while (millis() - startMillis < sampleWindow)
   {
     sample = analogRead(AMP_PIN);
@@ -32,7 +36,34 @@ void loop()
     }
   }
   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-  Serial.println(peakToPeak);
-  //double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+  //Serial.println(peakToPeak);
+  double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
   //Serial.println(volts);
+  return volts;
+}
+
+void sampleMic()
+{
+  // If completeSignal is full, delete first entry and add new one
+  if (signalIdx == completeSignal + signalLength - 1)
+  {
+    for (int i = 0; i < signalLength - 1; ++i)
+    {
+      completeSignal[i] = completeSignal[i+1]; // shifts all values left
+    }
+
+    // Sample once
+    completeSignal[signalIdx] = sampleOnce();
+  }
+  // If completeSignal is not full, sample until it is
+  else {
+    while (signalIdx != completeSignal + signalLength - 1) {
+      completeSignal[signalIdx] = sampleOnce();
+      signalIdx++;
+    }
+  }
+}
+void loop()
+{
+  sampleMic();
 }
